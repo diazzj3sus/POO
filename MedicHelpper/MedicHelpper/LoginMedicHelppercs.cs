99ae4f53b8e944f8d03c1e9e3f28e7895040cc61
diazzj3sus <36091637+diazzj3sus@users.noreply.github.com>
2020-09-21 16:36:14 -0600

    Login
    Se realizo la interfaz del login, también se realizo una clase ConexionSqlServer la cual establece una conexión con la bdd

--- MedicHelpper/MedicHelpper/LoginMedicHelppercs.cs
+++ MedicHelpper/MedicHelpper/LoginMedicHelppercs.cs
@@ -12,9 +12,64 @@ namespace MedicHelpper
 {
     public partial class LoginMedicHelppercs : Form
     {
+        ConexionSqlServer conectarbdd = new ConexionSqlServer();
         public LoginMedicHelppercs()
         {
             InitializeComponent();
         }
+        private void LoginMedicHelppercs_Load(object sender, EventArgs e)
+        {
+            conectarbdd.abrir();
+        }
+        private void txbContraseña_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Limpiar()
+        {
+            txbUsuario.Text = "Usuario:";
+            txbContraseña.Text = "Contraseña:";
+            txbContraseña.UseSystemPasswordChar = false;
+        }
+        private void txbUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txbUsuario.Text == "Usuario:")
+            {
+                txbUsuario.Clear();
+            }
+        }
+
+        private void txbUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txbUsuario.Text == "")
+            {
+                txbUsuario.Text = "Usuario:";
+            }
+        }
+
+        private void txbContraseña_Enter(object sender, EventArgs e)
+        {
+            if (txbContraseña.Text == "Contraseña:")
+            {
+                txbContraseña.Clear();
+                txbContraseña.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txbContraseña_Leave(object sender, EventArgs e)
+        {
+            if (txbContraseña.Text == "")
+            {
+                txbContraseña.Text = "Contraseña:";
+                txbContraseña.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+
     }
 }

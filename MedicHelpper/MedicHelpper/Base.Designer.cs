677ad046813b7591cef3ac75c4aea09f582c1697
Jeffrey Portillo <mixjeff@hotmail.com>
2020-10-02 15:07:33 -0600

    Prueba de cambio

--- MedicHelpper/MedicHelpper/Base.Designer.cs
+++ MedicHelpper/MedicHelpper/Base.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUE PEDO VA";
             // 
             // Base
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(502, 329);
+            this.Controls.Add(this.label1);
             this.Name = "Base";
             this.Text = "Base";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
